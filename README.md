28f3d71d58f755472c4c9f3c3b950b98d3849006
Muhammad Mabrouk <muhammad.mabrouk.web.designer@gmail.com>
2022-02-07 03:58:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# webroukBackTopScrollIndicator
+
+Webrouk Back Top Scroll Indicator is a lightweight native JavaScript web component for smooth back to top button with a circular progress bar which indicates the current reading progress when scrolling down/up the webpage.
+
+- No dependencies
+- Lightweight
+- Hides on top
+- Smooth Scroll
+
+### Demo
+---
+[CodePen Example](https://codepen.io/muhammad_mabrouk/full/oNoYQdr/)
+
+### Installation
+---
+Use [NPM](https://www.npmjs.com/package/webrouk-back-top-scroll-indicator/) to download and install it directly in to your project
+
+```sh
+$ npm install webrouk-back-top-scroll-indicator --save
+```
+
+or include js file manually
+
+```html
+<!-- webrouk-back-top-scroll-indicator component file -->
+<script src="webrouk-back-top-scroll-indicator.js"></script>
+```
+
+### Usage
+---
+Using webroukBackTopScrollIndicator is simple.
+
+```html
+<webrouk-back-top-scroll-indicator
+  title="Back to Top"
+  show-at="50">
+</webrouk-back-top-scroll-indicator>
+```
+
+### Options
+| Option | Type | Description | Default |
+| ----------- |    :----:   | ----------- |    :----:   |
+| show-at | `string` | Show the button at y position | `50` |
+
+### Style Customization
+---
+Expected CSS Variables from inside the component (optional).
+
+```css
+webrouk-back-top-scroll-indicator {
+  --primary-color: hsl(218, 95%, 54%);
+  --diameter-size: 46px;
+}
+```
+
+### License
+-------
+webroukBackTopScrollIndicator is licensed [MIT](https://choosealicense.com/licenses/mit/).
+It can be used for free and without any attribution, in any personal or commercial project.
---
+++ webrouk-back-top-scroll-indicator.js
@@ -0,0 +1,127 @@
+const webroukBackTopScrollIndicatorTemplate = document.createElement("template");
+webroukBackTopScrollIndicatorTemplate.innerHTML = `
+  <style>
+    :host,
+    :host * {
+      -webkit-box-sizing: border-box;
+      box-sizing: border-box;
+    }
+    :host {
+      --primary-color-fb: hsl(218, 95%, 54%);
+      --diameter-size-fb: 46px;
+    
+      position: fixed;
+      right: 25px;
+      bottom: 25px;
+      height: var(--diameter-size, var(--diameter-size-fb));
+      width: var(--diameter-size, var(--diameter-size-fb));
+      -webkit-transition: all 0.3s ease-in-out;
+      -o-transition: all 0.3s ease-in-out;
+      transition: all 0.3s ease-in-out;
+    }
+    :host(:not(:host(.show))) {
+      visibility: hidden;
+      opacity: 0;
+      -webkit-transform: translateY(10px);
+      -ms-transform: translateY(10px);
+      transform: translateY(10px);
+    }
+    .back-top-btn {
+      display: block;
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+      padding: 0;
+      border: 0;
+      outline: 0;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+    .back-top-btn svg path {
+      fill: none;
+      stroke: var(--primary-color, var(--primary-color-fb));
+      stroke-width: 5;
+    }
+    .back-top-btn::after {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      height: 40%;
+      width: 25%;
+      background-color: var(--primary-color, var(--primary-color-fb));
+      -webkit-clip-path: polygon(0 30%, 50% 0, 100% 30%, 94% 39%, 58% 17%, 58% 100%, 42% 100%, 42% 17%, 6% 39%);
+      clip-path: polygon(0 30%, 50% 0, 100% 30%, 94% 39%, 58% 17%, 58% 100%, 42% 100%, 42% 17%, 6% 39%);
+    }  
+  </style>
+
+  <button class="back-top-btn">
+    <svg width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+  </button>
+`;
+
+class WebroukBackTopScrollIndicator extends HTMLElement {
+
+  _BTSI_btn;
+  _BTSI_progressPath;
+  _BTSI_progressPathLength;
+  _BTSI_showAt;
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(webroukBackTopScrollIndicatorTemplate.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this._initBackTopScrollIndicator();
+    this._BTSI_btn.addEventListener("click", this._onClickingBackTop.bind(this));
+    document.addEventListener("scroll", this._onScrollingDocument.bind(this));
+  }
+
+  disconnectedCallback() {
+    this._BTSI_btn.removeEventListener("click", this._onClickingBackTop);
+    document.removeEventListener("scroll", this._onScrollingDocument);
+  }
+
+  // initialize back top scroll indicator
+  _initBackTopScrollIndicator() {
+    this._BTSI_showAt   = this.getAttribute("show-at") || 50;
+    this._BTSI_btn = this.shadowRoot.querySelector(".back-top-btn");
+    this._BTSI_progressPath = this._BTSI_btn.querySelector("path");
+    this._BTSI_progressPathLength = this._BTSI_progressPath.getTotalLength();
+
+    this._BTSI_progressPath.style.transition = this._BTSI_progressPath.style.WebkitTransition = "none";
+    this._BTSI_progressPath.style.strokeDasharray = `${this._BTSI_progressPathLength} ${this._BTSI_progressPathLength}`;
+    this._BTSI_progressPath.style.strokeDashoffset = this._BTSI_progressPathLength;
+    this._BTSI_progressPath.getBoundingClientRect();
+    this._BTSI_progressPath.style.transition = this._BTSI_progressPath.style.WebkitTransition = "stroke-dashoffset 10ms linear";
+
+    // actions on scrolling
+    this._onScrollingDocument();
+  }
+
+  // actions on scrolling
+  _onScrollingDocument() {
+    const scrollPosition = window.pageYOffset;
+    const pageHeight = document.body.clientHeight - window.innerHeight;
+    const scrollProgress = this._BTSI_progressPathLength - (scrollPosition * this._BTSI_progressPathLength) / pageHeight;
+
+    (scrollPosition >= this._BTSI_showAt) ? this.classList.add("show") : this.classList.remove("show");
+
+    this._BTSI_progressPath.style.strokeDashoffset = scrollProgress;
+  }
+
+  // actions on clicking back to top button
+  _onClickingBackTop() {
+    window.scroll({ top: 0, behavior: "smooth" });
+  }
+}
+
+window.customElements.define("webrouk-back-top-scroll-indicator", WebroukBackTopScrollIndicator);
